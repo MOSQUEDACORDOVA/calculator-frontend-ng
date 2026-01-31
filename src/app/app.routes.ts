aac0323fbe5b699ac8d15a44b1bf23800613cdf7
Isaac Mosqueda <elmosquedacordova@gmail.com>
2026-01-31 05:58:54 -0500

    initials

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '/calculator',
+    path: 'calculator',
     loadComponent: () =>
       import('./calculator/calculator').then((m) => m.Calculator),
   },
